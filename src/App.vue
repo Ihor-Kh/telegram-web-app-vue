ce10cf58887c9bccc68a39db47f00034acf107d5
IhorKh <holodenkoigor2401@gmail.com>
2022-10-15 20:51:56 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <h1>Тест приложения VUE в telegram bot</h1>
-  </div>
+	<div id="app">
+		<div>
+<!--			<div>Типа имя <span>{{tg.initDataUnsafe?.}}</span></div>-->
+			<button class="btn-tg">Закрыть</button>
+		</div>
+		<h1>Тест приложения VUE в telegram bot</h1>
+
+	</div>
 </template>
+
+<script>
+export default {
+	name: "test",
+	data(){
+		return {
+			tg: window.Telegram.WebApp
+		}
+	}
+}
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+}
+
+.btn-tg {
+	padding: 10px 15px;
+	background: var(--tg-theme-button-color);
+	color: var(--tg-theme-button-text-color);
+	border: none;
+	outline: none;
+	cursor: pointer;
 }
 
 nav {
-  padding: 30px;
+	padding: 30px;
 }
 
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
+	font-weight: bold;
+	color: #2c3e50;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
